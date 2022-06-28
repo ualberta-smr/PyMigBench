8e33792ed3a356f6d4577dfd17c2374193e159f6
Mohayemin <mohayeminul.islam@gmail.com>
2022-06-28 05:56:50 -0600

    refactor: change items list to dictionary

--- code/db/Db.py
+++ code/db/Db.py
@@ -10,31 +10,32 @@ from db.Migration import Migration
 
 
 class Db:
-    code_changes: list[CodeChange]
-    migrations: list[Migration]
-    lib_pairs: list[LibPair]
-    _mapping: dict[str, list]
+    code_changes: dict[str, CodeChange]
+    migrations: dict[str, Migration]
+    lib_pairs: dict[str, LibPair]
+    _mapping: dict[str, dict[str]]
 
     def __init__(self, data_root: str):
         self.data_root = data_root
 
     def load(self):
-        self.code_changes = self.load_list("codechange", CodeChange)
-        self.migrations = self.load_list("migration", Migration)
-        self.lib_pairs = self.load_list("libpair", LibPair)
+        self.code_changes = self.load_items("codechange", CodeChange)
+        self.migrations = self.load_items("migration", Migration)
+        self.lib_pairs = self.load_items("libpair", LibPair)
         self._mapping = {
             CodeChangeKey: self.code_changes,
             MigrationKey: self.migrations,
             LibPairKey: self.lib_pairs,
         }
 
-    def get_list(self, key: str):
-        return self._mapping[key]
+    def get_list(self, type_key: str):
+        return self._mapping[type_key].values()
 
-    def load_list(self, data_folder, data_type):
+    def load_items(self, data_folder, data_type):
         paths = Path(self.data_root, data_folder).glob("*.yaml")
-        items = [self.load_item(p, data_type) for p in paths]
-        return items
+        items = (self.load_item(p, data_type) for p in paths)
+        dict = {item.id: item for item in items}
+        return dict
 
     def load_item(self, yaml_path: Path, ctor: Type):
         with open(yaml_path) as f:
